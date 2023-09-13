d8be681612e12645eca5681f98cf2ad542b829a7
win <bluizsilva37@gmail.com>
2023-09-13 17:37:39 -0300

    01

--- app/src/main/java/com/example/instagramclonecompose/MainActivity.kt
+++ app/src/main/java/com/example/instagramclonecompose/MainActivity.kt
@@ -17,27 +17,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             InstagramCloneComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InstagramCloneComposeTheme {
-        Greeting("Bruno")
-    }
-}
